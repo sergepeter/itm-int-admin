a619304bdc962e05850b3b3faec34c342ed84d99
Serge Peter <serge.peter@nagra.com>
2015-02-26 10:44:56 +0100

    Upgrade Resteasy lib

---
+++ src/main/java/com/masict/itm/data/service/RestApplication.java
@@ -0,0 +1,9 @@
+package com.masict.itm.data.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+
+}
